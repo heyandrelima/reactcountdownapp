33ff54ac7f186507cc1e3bc3abd33d0a2857e53a
Andre Lima <hello@andrelima.space>
2018-11-06 23:59:31 -0200

    Updates

---
+++ precache-manifest.dc0c029cc4b770ea0d8d92a514a1494b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d6e52827e957dec5316",
+    "url": "/reactcountdownapp/static/js/runtime~main.8d6e5282.js"
+  },
+  {
+    "revision": "c05fce2452f9a33cff24",
+    "url": "/reactcountdownapp/static/js/main.c05fce24.chunk.js"
+  },
+  {
+    "revision": "9447623344f0ea2f9d25",
+    "url": "/reactcountdownapp/static/js/1.94476233.chunk.js"
+  },
+  {
+    "revision": "c05fce2452f9a33cff24",
+    "url": "/reactcountdownapp/static/css/main.07fd48fc.chunk.css"
+  },
+  {
+    "revision": "353ad9a1796e984de1bb673da818413d",
+    "url": "/reactcountdownapp/index.html"
+  }
+];
